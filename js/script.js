bc428591f988ff73d08d3b37742ad8e461a4b659
Iris Enid P <100088476+irisep@users.noreply.github.com>
2022-08-26 15:34:19 +0300

    fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const overview = document.querySelector(".overview");
 const username = "irisep";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -14,13 +15,29 @@ const displayUserInfo = function (data) {
     div.classList.add("user-info");
     div.innerHTML = `
     <figure>
-      <img alt="user avatar" src=${} />
+      <img alt="user avatar" src=${data.avatar_url} />
     </figure>
     <div>
-      <p><strong>Name:</strong> ${}</p>
-      <p><strong>Bio:</strong> ${}</p>
-      <p><strong>Location:</strong> ${}</p>
-      <p><strong>Number of public repos:</strong> ${}</p>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
     overview.append(div);
+    gitRepos();
 };
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
